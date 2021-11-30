bb9e860646f8f835db17b8a649d3fff78ca959fd
Jessee Holmes <jesseeholmes@CRC0090eeHolmes.local>
2021-11-30 15:21:49 -0600

    Section 5: Completed

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,7 +5,6 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-  const [state, setState] = useState("start");
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -21,7 +20,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -30,58 +29,47 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
-    setState("start");
   };
 
   return (
-    <div>
-      {state === "start" && (
-        <div className="new-expsense__actions">
-          <button type="submit" onClick={() => setState("adding")}>
-            Add New Expense
-          </button>
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expsense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
-      )}
-
-      {state === "adding" && (
-        <form onSubmit={submitHandler}>
-          <div className="new-expense__controls">
-            <div className="new-expsense__control">
-              <label>Title</label>
-              <input
-                type="text"
-                value={enteredTitle}
-                onChange={titleChangeHandler}
-              />
-            </div>
-            <div className="new-expsense__control">
-              <label>Amount</label>
-              <input
-                type="number"
-                min="0.01"
-                step="0.01"
-                value={enteredAmount}
-                onChange={amountChangeHandler}
-              />
-            </div>
-            <div className="new-expsense__control">
-              <label>Date</label>
-              <input
-                type="date"
-                min="2019-01-01"
-                max="2022-12-31"
-                value={enteredDate}
-                onChange={dateChangeHandler}
-              />
-            </div>
-          </div>
-          <div className="new-expsense__actions">
-            <button onClick={() => setState("start")}>Cancel</button>
-            <button type="submit">Add Expense</button>
-          </div>
-        </form>
-      )}
-    </div>
+        <div className="new-expsense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expsense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expsense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
   );
 };
 
